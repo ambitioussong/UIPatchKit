4dc18ca21ea79a7671bc92956215ec3c195e0caa
I'mYourDad <qinzhen_xu@pinssible.com>
2017-03-05 17:18:19 +0800

    Add .h

---
+++ UIPatchKit/UIPatchKit/UIPatchKit.h
@@ -0,0 +1,24 @@
+//
+//  UIPatchKit.h
+//  UIPatchKit
+//
+//  Created by Mayqiyue on 05/03/2017.
+//  Copyright © 2017 mayqiyue. All rights reserved.
+//
+
+#ifndef UIPatchKit_h
+#define UIPatchKit_h
+
+#import "UIApplication+PKAdd.h"
+#import "UIBlurEffect+PKAdd.h"
+#import "UIButton+PKAdd.h"
+#import "UICollectionViewCell+PKAdd.h"
+#import "UICollectionViewFlowLayout+PKAdd.h"
+#import "UIColor+PKAdd.h"
+#import "UIImage+PKAdd.h"
+#import "UINavigationController+PKAdd.h"
+#import "UIView+PKAdd.h"
+#import "UIViewController+PKAdd.h"
+
+
+#endif /* UIPatchKit_h */
